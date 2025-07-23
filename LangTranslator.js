46e11fcbae73eba4784a2ffac275f74ad312c492
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-07-23 22:35:53 +0530

    edit files

--- LangTranslator.js
+++ LangTranslator.js
@@ -2,6 +2,7 @@ const selectTag = document.querySelectorAll("select");
 
 selectTag.forEach((tag)=>{
    for(const countriesCode in countries){
-   let option = `<option value="${countriesCode}">${countries[countriesCode]}</option>`
+   let option = `<option value="${countriesCode}">${countries[countriesCode]}</option>`;
+   tag.insertAdjacentHTML("beforeend", option)
 }
 });
