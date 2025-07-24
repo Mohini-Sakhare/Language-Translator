312d75fb906b19add047608b697b8bb61e08f220
Mohini-Sakhare <sakharemohini123@gmail.com>
2025-07-24 23:19:09 +0530

    add code in js

--- LangTranslator.js
+++ LangTranslator.js
@@ -1,8 +1,31 @@
 const selectTag = document.querySelectorAll("select");
+const translateBtn = document.querySelector("#Translator")
+const fromText = document.querySelector("#fromText");
+const toText = document.querySelector("#toText");
 
-selectTag.forEach((tag)=>{
+selectTag.forEach((tag,id)=>{
    for(const countriesCode in countries){
-   let option = `<option value="${countriesCode}">${countries[countriesCode]}</option>`;
+    let selected ;
+    if(id == 0 && countriesCode == "en-IN"){
+        selected= "selected";
+    }else if(id == 1 && countriesCode == "hi-IN"){
+        selected= "selected";
+    }
+   let option = `<option value="${countriesCode}" ${selected}>${countries[countriesCode]}</option>`;
    tag.insertAdjacentHTML("beforeend", option)
 }
 });
+
+translateBtn.addEventListener(("click"), ()=>{
+   let Text = fromText.value,
+   translateFrom = selectTag[0].value,
+   translateTo = selectTag[1].value;
+
+//    let URL =` https://api.mymemory.translated.net/get?q=${Text}!&langpair=${translateFrom}|${translateTo}`;
+
+//    fetch(URL).then(res => res.json()).then(data =>{
+
+//    })
+});
+
+
